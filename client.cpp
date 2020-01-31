0ec0af1721d270c09e127c64678a6c7dde795ad5
Israel-hakoua <56992624+Israel-hakoua@users.noreply.github.com>
2020-01-31 01:23:54 +0100

    class client ok

---
+++ client.cpp
@@ -0,0 +1,94 @@
+#include "client.h"
+#include <string>
+#include <vector>
+#include "produit.h"
+
+using namespace std;
+
+client::client(int id_, string prenom_, string nom_) : id(id_), nom(nom_), prenom(prenom_){
+}																															
+
+int client::getId(){ 
+	return id;
+}
+
+string client::getNom(){
+	return nom;
+}	
+
+string client::getPrenom(){
+	return prenom;
+}
+
+vector<produit> client::getPanier(){
+	return panier_achat;
+}
+
+void client::addPdtPanier(produit prod){ 
+	panier_achat.push_back(prod);
+}
+
+void client::supPdtPanier(string nom_produit){
+	for(int i=0; i< panier_achat.size(); i++){
+		produit prod = panier_achat.at(i);
+		if(prod.getTitre() == nom_produit){
+			panier_achat.erase(panier_achat.begin() + i);
+		}
+	}
+}
+
+void client::videPanier(){
+	panier_achat.clear();
+}
+
+void client::modifQuantitePanier(string nom_produit, int quantite){
+	for(int i=0; i< panier_achat.size(); i++){
+		produit prod = panier_achat.at(i);
+		if(prod.getTitre() == nom_produit){
+			(panier_achat.at(i)).setQuantite(quantite);
+		}
+	}
+}
+
+ostream& operator<<(ostream &flux, client *client1){
+	//////////////////////////// pour la presentation
+	string taille; 
+	int n;
+	////////////////////////////
+
+	vector<produit> panier;
+	panier = client1->getPanier();
+	flux << " ____________________________________________________________________________" << endl;
+	flux << "| Information Client                                                         |" << endl;
+	flux << "| Identifiant : " << client1->getId();
+	taille = to_string(client1->getId());
+	n=61-taille.size();
+	for (int i =0; i<n; i++){
+		flux << " ";
+	}
+	flux << "|" << endl << "| Nom : " << client1->getNom();
+	taille =client1->getNom();
+	n=69-taille.size();
+	for (int i =0; i<n; i++){
+		flux << " ";
+	}
+
+	flux << "|" << endl << "| Prenom : " << client1->getPrenom() ;
+	taille =client1->getPrenom();
+	n=66-taille.size();
+	for (int i =0; i<n; i++){
+		flux << " ";
+	}
+
+	flux << "|" << endl;
+	flux << "|----------------------------------------------------------------------------|" << endl;
+	flux << "| Produits  :                                                                |" << endl;
+	for(int i = 0; i < panier.size(); i++){
+		produit prod = panier.at(i);
+		cout<< &prod;
+	}
+
+	flux << "|____________________________________________________________________________|" << endl;
+	flux << endl;
+	return flux;
+}
---
+++ client.h
@@ -0,0 +1,33 @@
+#ifndef _client_h
+#define _client_h
+
+#include <iostream>
+#include <string>
+#include "produit.h"
+#include <vector>
+
+using namespace std;
+
+class client {
+public:
+	client(int id_, string prenom_, string nom_);
+	int getId();
+	string getNom();
+	string getPrenom();
+	vector<produit> getPanier();
+	void addPdtPanier(produit prod);
+	void videPanier();
+	void modifQuantitePanier(string nom_produit, int quantite);
+	void supPdtPanier(string nom_produit);
+	
+	friend ostream& operator<<(ostream &flux, client *client1);
+
+private:
+	int id;
+	string nom;
+	string prenom;
+	vector<produit> panier_achat;
+};
+
+
+#endif
